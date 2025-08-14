df16a0b0fcba0c4224bbb563837c90a05b91156e
nephm <160319807+nephm@users.noreply.github.com>
2025-08-14 17:07:23 +1000

    Project setup with Spring Boot

---
+++ src/main/java/com/uts/Online/Booking/App/OnlineBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.uts.Online.Booking.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uts/Online/Booking/App/OnlineBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uts.Online.Booking.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
