5e01a22019e5f75599dd6f942321337731e70f03
bwe77 <137379557+bwe77@users.noreply.github.com>
2025-09-03 18:02:47 +1000

    pullrequest#1

--- src/main/java/com/uts/Online/Booking/App/controller/RegisterController.java
+++ src/main/java/com/uts/Online/Booking/App/controller/RegisterController.java
@@ -1,53 +1,53 @@
-package com.uts.Online.Booking.App.controller;
+// package com.uts.Online.Booking.App.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-import com.uts.Online.Booking.App.DAO.UserDAO;
-import com.uts.Online.Booking.App.model.User;
+// import com.uts.Online.Booking.App.DAO.UserDAO;
+// import com.uts.Online.Booking.App.model.User;
 
-import jakarta.servlet.http.HttpSession;
+// import jakarta.servlet.http.HttpSession;
 
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 
 
-@Controller
-public class RegisterController {
+// @Controller
+// public class RegisterController {
     
-    private final UserDAO userDAO;
+//     private final UserDAO userDAO;
 
-    RegisterController(UserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
+//     RegisterController(UserDAO userDAO) {
+//         this.userDAO = userDAO;
+//     }
 
-    @GetMapping("/register")
-    public String registerPage(){
-        return "register";
-    }
+//     @GetMapping("/register")
+//     public String registerPage(){
+//         return "register";
+//     }
 
-    @PostMapping("/register")
-    public String RegisterUser(@RequestParam("firstName") String fname, @RequestParam("lastName") String lname, @RequestParam("email") String email, @RequestParam("password") String password, 
-    @RequestParam("phoneNumber") int phoneNumber, Model m, HttpSession session){
+//     @PostMapping("/register")
+//     public String RegisterUser(@RequestParam("firstName") String fname, @RequestParam("lastName") String lname, @RequestParam("email") String email, @RequestParam("password") String password, 
+//     @RequestParam("phoneNumber") int phoneNumber, Model m, HttpSession session){
 
-        if(userDAO.findByEmail(email).isPresent()){
-            m.addAttribute("error", "Email already exists!");
-            return "register";
-        } else{
-            User newUser = new User();
-            newUser.setFirstName(fname);
-            newUser.setlastName(lname);
-            newUser.setEmail(email);
-            newUser.setPassword(password);
-            newUser.setPhoneNumber(phoneNumber);
-            userDAO.save(newUser);
+//         if(userDAO.findByEmail(email).isPresent()){
+//             m.addAttribute("error", "Email already exists!");
+//             return "register";
+//         } else{
+//             User newUser = new User();
+//             newUser.setFirstName(fname);
+//             newUser.setlastName(lname);
+//             newUser.setEmail(email);
+//             newUser.setPassword(password);
+//             newUser.setPhoneNumber(phoneNumber);
+//             userDAO.save(newUser);
 
-            session.setAttribute("user", newUser);
+//             session.setAttribute("user", newUser);
 
-            return "redirect:/main";
-        }
-    }
+//             return "redirect:/main";
+//         }
+//     }
 
 
-}
+// }
--- src/main/java/com/uts/Online/Booking/App/model/User.java
+++ src/main/java/com/uts/Online/Booking/App/model/User.java
@@ -1,83 +1,83 @@
-package com.uts.Online.Booking.App.model;
+// package com.uts.Online.Booking.App.model;
 
-import jakarta.persistence.*;
+// import jakarta.persistence.*;
 
-@Entity
-@Table(name ="users")
-public class User {
+// @Entity
+// @Table(name ="users")
+// public class User {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    private String firstName;
-    private String lastName;
+//     private String firstName;
+//     private String lastName;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+//     @Column(nullable = false, unique = true)
+//     private String email;
 
-    @Column(nullable = false)
-    private String password;
-    private int phoneNumber;
+//     @Column(nullable = false)
+//     private String password;
+//     private int phoneNumber;
 
-    // Constructors
-    public User() {}
+//     // Constructors
+//     public User() {}
 
-    public User(Long id, String firstName, String lastName, String email, String password, int phoneNumber) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-    }
+//     public User(Long id, String firstName, String lastName, String email, String password, int phoneNumber) {
+//         this.id = id;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.email = email;
+//         this.password = password;
+//         this.phoneNumber = phoneNumber;
+//     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
+//     // Getters and Setters
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public String getFirstName() {
-        return firstName;
-    }
+//     public String getFirstName() {
+//         return firstName;
+//     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+//     public void setFirstName(String firstName) {
+//         this.firstName = firstName;
+//     }
 
-    public String getlastName() {
-        return lastName;
-    }
+//     public String getlastName() {
+//         return lastName;
+//     }
 
-    public void setlastName(String lastName) {
-        this.lastName = lastName;
-    }
+//     public void setlastName(String lastName) {
+//         this.lastName = lastName;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
 
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
+//     public int getPhoneNumber() {
+//         return phoneNumber;
+//     }
 
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-}
+//     public void setPhoneNumber(int phoneNumber) {
+//         this.phoneNumber = phoneNumber;
+//     }
+// }
